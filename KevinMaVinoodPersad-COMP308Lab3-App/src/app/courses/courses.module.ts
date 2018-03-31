71a96451a430b0c5d8d441ab0b9a8418ae9d2922
Kevin Ma <kbma.kevin@gmail.com>
2018-03-31 18:01:44 -0400

    fix -> merged wrong course module file previously

--- KevinMaVinoodPersad-COMP308Lab3-App/src/app/courses/courses.module.ts
+++ KevinMaVinoodPersad-COMP308Lab3-App/src/app/courses/courses.module.ts
@@ -1,44 +1,21 @@
-/**
- * 
- * @file        courses.server.routes.js
- * @description defines the routes for the courses entity
- * @author      Kevin Ma, Vinood Persad
- * @date        2018.03.21
- * 
- */
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
-const express = require('express');
-const router = express.Router();
-const mongoose = require('mongoose');
+import { Routes, RouterModule } from '@angular/router';
+import { CoursesComponent } from './courses.component';
+import { ListCoursesComponent } from './list/listcourses.component';
+import { CreateCourseComponent } from './create/createcourse.component';
+import { CoursesService } from './courses.service';
+import { routing } from '../app.routing';
 
-// student object created from the Schema / model
-const Student = require('../models/students.server.model');
-const Course = require('../models/courses.server.model');
-const coursesController = require("../controllers/courses.server.controller");
-
-router.post('/', (req, res, next) => {
-    coursesController.CreateCourse(req, res, next);
-});
-
-router.get('/', (req, res, next) => {
-    coursesController.GetCourses(req, res, next);
-});
-
-router.get('/delete/:code', (req, res, next) => {
-    coursesController.DeleteCourse(req, res, next);
-});
-
-router.get('/:id', (req, res, next) => {
-    coursesController.GetOneCourse(req, res, next);
-});
-
-router.post('/removestudent', (req, res, next) => {
-    coursesController.RemoveStudentFromCourse(req, res, next);
-});
-
-router.post('/updatecourse', (req, res, next) => {
-    coursesController.UpdateCourse(req, res, next);
-});
-
-
-module.exports = router;
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        routing
+    ],
+    declarations: [CoursesComponent, ListCoursesComponent, CreateCourseComponent],
+    providers: [CoursesService]
+})
+export class CoursesModule { }
